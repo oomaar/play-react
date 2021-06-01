926efb60e94ac4f22391f2df1b0636ab3284b0a9
Omar <omaar_5@hotmail.com>
2021-06-01 19:30:36 +0200

    feat: setup Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import styled from "styled-components/macro";
+import { Navbar } from "./components";
+
 const App = () => {
   return (
-    <div>
-      Play React
-    </div>
+    <Application>
+      <Navbar />
+    </Application>
   );
 };
 
 export default App;
+
+const Application = styled.div``;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import {
+    Nav,
+    Container,
+} from "./styledNavbar";
+
+const Navbar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Navbar;
