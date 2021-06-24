aeb31ef4049ed624c0b75af609ba090330d15e79
Omar <omaar_5@hotmail.com>
2021-06-24 18:07:02 +0200

    feat: complete Form (2 ways of submit, no back)

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,61 @@
+import { useForm } from '@formspree/react';
+// import { ValidationError } from '@formspree/react';
 import {
     Container,
     SubContainer,
+    FormForm,
+    FormGroup,
+    FormLabel,
+    FormInput,
+    FormTextareaGroup,
+    TextAreaLabel,
+    FormTextArea,
+    FormSubmit,
 } from "./styledForm";
 
 const Form = () => {
+    const [state, handleSubmit] = useForm("xoqylqqb");
+
+    const formStructure = () => {
+        return (
+            <SubContainer>
+                {/* <FormForm action="https://formspree.io/f/xoqylqqb" method="POST"> */}
+                <FormForm onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <FormInput type="text" id="firstName" name="firstName" required />
+                        <FormLabel htmlFor="firstName"><span>First Name</span></FormLabel>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormInput type="text" id="lastName" name="lastName" required />
+                        <FormLabel htmlFor="lastName"><span>Last Name</span></FormLabel>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormInput type="email" id="email" name="email" required />
+                        <FormLabel htmlFor="email"><span>Email</span></FormLabel>
+                    </FormGroup>
+                    <FormTextareaGroup>
+                        <FormTextArea name="message" id="message" cols="30" rows="10" required></FormTextArea>
+                        <TextAreaLabel htmlFor="message"><span>Message</span></TextAreaLabel>
+                    </FormTextareaGroup>
+
+                    <FormSubmit type="submit" disabled={state.submitting}>Submit</FormSubmit>
+                </FormForm>
+            </SubContainer>
+        );
+    }
+
+    if (state.succeeded) {
+        return (
+            <>
+                {formStructure()}
+                <p>Thanks for joining!</p>
+            </>
+        );
+    };
+
     return (
         <Container>
-            <SubContainer>
-                Form
-            </SubContainer>
+            {formStructure()}
         </Container>
     );
 };
--- src/components/Form/styledForm.js
+++ src/components/Form/styledForm.js
@@ -6,5 +6,138 @@ export const Container = styled.div`
 
 export const SubContainer = styled.div`
   background-color: #fff;
-  color: #000;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+`;
+
+export const FormForm = styled.form`
+  border-radius: 10px;
+  background-color: hsla(0, 0%, 0%, 0.7);
+  color: #fff;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+`;
+
+export const FormGroup = styled.div`
+    position: relative;
+    margin: 15px 0;
+    width: 100%;
+`;
+
+export const FormLabel = styled.label`
+    position: absolute;
+    border: 1px solid grey;
+    border-radius: 5px;
+    left: 0px;
+    width: 100%;
+    height: 65px;
+    pointer-events: none;
+
+    span {
+      position: absolute;
+      top: 20%;
+      left: 7px;
+      transition: all 0.1s ease;
+      font-size: 16px;
+    }
+`;
+
+export const FormInput = styled.input`
+    background: transparent;
+    border: 0;
+    font-size: 16px;
+    height: 65px;
+    padding: 30px 10px 0;
+    outline: 0;
+    width: 100%;
+    color: #fff;
+
+    :focus {
+      border: 2px solid hsl(100, 80%, 50%);
+      border-radius: 5px;
+   }
+
+   :focus + ${FormLabel} span {
+      transform: translateY(-70%);
+      color: hsl(100, 80%, 50%);
+      font-size: 12px;
+      left: 5px;
+      top: 25%;
+    }
+
+    :valid + ${FormLabel} span {
+        transform: translateY(-70%);
+        font-size: 12px;
+        left: 5px;
+        top: 25%;
+    }
+`;
+
+export const FormTextareaGroup = styled.div`
+  position: relative;
+`;
+
+export const TextAreaLabel = styled.label`
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 200px;
+    height: 100%;
+    pointer-events: none;
+
+    span {
+      position: absolute;
+      top: 10px;
+      left: 0px;
+      transition: all 0.1s ease;
+    }
+`;
+
+export const FormTextArea = styled.textarea`
+    background: transparent;
+    border: 1px solid grey;
+    border-radius: 5px;
+    font-size: 16px;
+    padding: 30px 10px 0;
+    outline: 0;
+    width: 100%;
+    color: #fff;
+
+    :focus {
+      border: 2px solid hsl(100, 80%, 50%);
+      border-radius: 5px;
+   }
+
+    :focus + ${TextAreaLabel} span {
+      transform: translateY(-70%);
+      color: hsl(100, 80%, 50%);
+      font-size: 12px;
+      left: 0;
+      top: 5px;
+    }
+
+    :valid + ${TextAreaLabel} span {
+        transform: translateY(-70%);
+        font-size: 12px;
+        left: 0;
+        top: 5px;
+    }
+`;
+
+export const FormSubmit = styled.button`
+  border: 0;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: hsl(100, 80%, 50%);
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  margin-top: 10px;
+
+  :hover {
+      opacity: 0.8;
+  }
 `;
